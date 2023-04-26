225bbd00b960f3ce0fb8f8b29dd233ccc1d379da
Chaeyeon Seo <ssallyseo@gmail.com>
2023-04-27 01:36:57 +0900

    0426-assignment

--- week3_0426/assignment.py
+++ week3_0426/assignment.py
@@ -1,2 +1,19 @@
 # 파스칼의 삼각형
 
+def pascal(n):
+    m = n - 1 # 리스트 인덱스는 0부터 시작하므로 맞춰주기
+    if m == 0: # 0번째 리스트, 즉 1번 줄
+        return [1]
+    
+    else:
+        before = pascal(n-1) #  이전 리스트
+        p = []
+        p.append(1) # 가장 앞에 1
+        
+        for i in range(1,m):
+            p.append(before[i-1] + before[i]) 
+        p.append(1) # 마지막에 1
+        return p
+
+print(pascal(7))
+
